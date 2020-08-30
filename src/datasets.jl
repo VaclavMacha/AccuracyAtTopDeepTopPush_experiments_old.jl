293aa002f23aab84d991d52446ac3ee2913d1f95
Vaclav Macha <vaclav.macha92@gmail.com>
2020-08-30 11:49:29 +0200

    Added build_network

--- src/datasets.jl
+++ src/datasets.jl
@@ -1,12 +1,14 @@
 using Flux
-import MLDatasets
 using MLDataPattern
+using Random
 
-abstract type Dataset; end
+import MLDatasets
 
 # -------------------------------------------------------------------------------
 # Utility functions
 # -------------------------------------------------------------------------------
+abstract type Dataset; end
+
 function load(D::Type{<:Dataset}, T = Float32; kwargs...)
     return reshape_dataset.(load_raw(D, T); kwargs...)
 end
@@ -30,9 +32,10 @@ function reshape_samples(x::AbstractArray{T, 3}) where T
 end
 
 # -------------------------------------------------------------------------------
-# Image datasets
+# MNIST-like datasets
 # -------------------------------------------------------------------------------
-abstract type MNIST <: Dataset; end
+abstract type AbstractMNIST <: Dataset end
+abstract type MNIST <: AbstractMNIST end
 
 function load_raw(::Type{MNIST}, T)
     train = MLDatasets.MNIST.traindata(T)
@@ -41,7 +44,7 @@ function load_raw(::Type{MNIST}, T)
     return train, test
 end
 
-abstract type FashionMNIST <: Dataset; end
+abstract type FashionMNIST <: AbstractMNIST end
 
 function load_raw(::Type{FashionMNIST}, T)
     train = MLDatasets.FashionMNIST.traindata(T)
@@ -50,7 +53,29 @@ function load_raw(::Type{FashionMNIST}, T)
     return train, test
 end
 
-abstract type CIFAR10 <: Dataset; end
+function build_network(::Type{<:AbstractMNIST}; seed = 1234)
+    Random.seed!(seed)
+
+    return Chain(
+        # First convolution
+        Conv((5, 5), 1=>20, stride=(1,1), relu),
+        MaxPool((2,2)),
+
+        # Second convolution
+        Conv((5, 5), 20=>50, stride=(1,1), relu),
+        MaxPool((2,2)),
+
+        flatten,
+        Dense(800, 500),
+        Dense(500, 1)
+    )
+end
+
+# -------------------------------------------------------------------------------
+# CIFAR dataset
+# -------------------------------------------------------------------------------
+abstract type AbstractCIFAR <: Dataset end
+abstract type CIFAR10 <: AbstractCIFAR end
 
 function load_raw(::Type{CIFAR10}, T)
     train = MLDatasets.CIFAR10.traindata(T)
@@ -59,7 +84,7 @@ function load_raw(::Type{CIFAR10}, T)
     return train, test
 end
 
-abstract type CIFAR20 <: Dataset; end
+abstract type CIFAR20 <: AbstractCIFAR end
 
 function load_raw(::Type{CIFAR20}, T)
     train = MLDatasets.CIFAR100.traindata(T)[[1,2]]
@@ -68,7 +93,7 @@ function load_raw(::Type{CIFAR20}, T)
     return train, test
 end
 
-abstract type CIFAR100 <: Dataset; end
+abstract type CIFAR100 <: AbstractCIFAR end
 
 function load_raw(::Type{CIFAR100}, T)
     train = MLDatasets.CIFAR100.traindata(T)[[1,3]]
@@ -77,7 +102,32 @@ function load_raw(::Type{CIFAR100}, T)
     return train, test
 end
 
-abstract type SVHN2 <: Dataset; end
+function build_network(::Type{<:AbstractCIFAR}; seed = 1234)
+    Random.seed!(seed)
+
+    return Chain(
+        # First convolution, operating upon a 32x32 image
+        Conv((3, 3), 3=>64, pad=(1,1), relu),
+        MaxPool((2,2)),
+
+        # Second convolution, operating upon a 16x16 image
+        Conv((3, 3), 64=>128, pad=(1,1), relu),
+        MaxPool((2,2)),
+
+        # Third convolution, operating upon a 4x4 image
+        Conv((3, 3), 128=>128, pad=(1,1), relu),
+        MaxPool((2,2)),
+
+        flatten,
+        Dense(2048, 1)
+    )
+end
+
+# -------------------------------------------------------------------------------
+# SVHN2 dataset
+# -------------------------------------------------------------------------------
+abstract type AbstractSVHN2 <: Dataset end
+abstract type SVHN2 <: AbstractSVHN2 end
 
 function load_raw(::Type{SVHN2}, T)
     train = MLDatasets.SVHN2.traindata(T)
@@ -86,7 +136,7 @@ function load_raw(::Type{SVHN2}, T)
     return train, test
 end
 
-abstract type SVHN2Full <: Dataset; end
+abstract type SVHN2Full <: AbstractSVHN2 end
 
 function load_raw(::Type{SVHN2Full}, T)
     x1, y1 = MLDatasets.SVHN2.traindata(T)
@@ -96,4 +146,25 @@ function load_raw(::Type{SVHN2Full}, T)
     test = MLDatasets.SVHN2.testdata(T)
 
     return train, test
+end
+
+function build_network(::Type{<:AbstractSVHN2}; seed = 1234)
+    Random.seed!(seed)
+
+    return Chain(
+        # First convolution, operating upon a 32x32 image
+        Conv((3, 3), 3=>64, pad=(1,1), relu),
+        MaxPool((2,2)),
+
+        # Second convolution, operating upon a 16x16 image
+        Conv((3, 3), 64=>128, pad=(1,1), relu),
+        MaxPool((2,2)),
+
+        # Third convolution, operating upon a 4x4 image
+        Conv((3, 3), 128=>128, pad=(1,1), relu),
+        MaxPool((2,2)),
+
+        flatten,
+        Dense(2048, 1)
+    )
 end
