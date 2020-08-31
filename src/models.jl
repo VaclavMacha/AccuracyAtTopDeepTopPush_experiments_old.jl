d691ca75baed6d143afc3407d9f1150113750d5c
Vaclav Macha <vaclav.macha92@gmail.com>
2020-08-31 20:15:28 +0200

    Improved model loss functions

--- src/models.jl
+++ src/models.jl
@@ -18,9 +18,12 @@ weights(y, s) = oftype(s, y ./ sum(y .== 1) .+ (1 .- y) ./ sum(y .== 0))
 function build_loss(::Type{BaseLine}, arg, surrogate, reg)
 
     function loss(x, y, model, pars)
-        s = model(x)
+        return loss(y, model(x), pars)
+    end
+
+    function loss(y, s, pars)
         w = weights(y, s)
-        return binarycrossentropy(sigmoid.(s), y; agg = x -> sum(w .* x)) + eltype(x)(reg) * sum(sqsum, pars)
+        return binarycrossentropy(sigmoid.(s), y; agg = x -> sum(w .* x)) + eltype(s)(reg) * sum(sqsum, pars)
     end
     return loss
 end
@@ -31,7 +34,7 @@ abstract type PatMat <: Model end
 abstract type PatMatNP <: Model end
 
 build_threshold(::Type{DeepTopPush}, arg) = Maximum(; samples = NegSamples)
-build_threshold(::Type{PatMat}, τ::Real) = Quantile(τ; samples = AllSamples)
+build_threshold(::Type{PatMat}, τ::Real) = Quantile(τ; samples = AllSamples, rev = true)
 build_threshold(::Type{PatMatNP}, τ::Real) = FPRate(τ)
 
 sqsum(x) = sum(abs2, x)
@@ -40,9 +43,12 @@ function build_loss(type, arg, surrogate, reg)
     thres = build_threshold(type, arg)
 
     function loss(x, y, model, pars)
-        s = model(x)
+        return loss(y, model(x), pars)
+    end
+
+    function loss(y, s, pars)
         t = threshold(thres, y, s)
-        return fnr(y, s, t, surrogate) + eltype(x)(reg) * sum(sqsum, pars)
+        return fnr(y, s, t, surrogate) + eltype(s)(reg) * sum(sqsum, pars)
     end
     return loss
 end
